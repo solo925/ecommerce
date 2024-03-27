f14c29e71daa9742b176cb0931eb746e45446baa
davinci <davinci@localhost.localdomain>
2024-03-27 08:05:19 -0400

    add 2

--- store/views.py
+++ store/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
-    context ={}
+    products = Product.objects.all()
+    context ={'products':products}
     return render(request,'store/Store.html',context)
 
 def cart(request):
-    context ={}
+    if request.user.is_authenticated:
+        customer = Customer.objects.get()
+        order,created = Order.objects.get_or_create(customer = customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context ={'items':items}
     return render(request,'store/Cart.html',context)
 
 def checkout(request):
